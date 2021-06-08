6fd083dc475bb19d0211ca47463113fe8519227d
石丸大貴 <hirokiishimaru6132@gmail.com>
2021-06-08 16:45:07 +0900

    constと数値型を勉強した

---
+++ section01/practice03.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+// const
+const Pi = 3.14
+
+const (
+	Username = "test_user"
+	Password = "test_pass"
+)
+
+func main() {
+	fmt.Println(Pi, Username, Password)
+}
